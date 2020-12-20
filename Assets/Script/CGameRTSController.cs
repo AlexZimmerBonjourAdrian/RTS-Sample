73e2d55969decdd26ff278cb513b0116a1b841fd
AexZimmer <alxzimmer.zimmer1@gmail.com>
2020-12-20 01:34:06 -0300

    Unit

---
+++ Assets/Script/CGameRTSController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class CGameRTSController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector3 starPosition;
+    private void Update()
+    {
+        /*
+        if(Input.GetButtonDown(0))
+        {
+           
+        }
+        */
+    }
+}
---
+++ Assets/Script/CUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CUtil : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Camera sceneCamera;
+    // Start is called before the first frame update
+    private Plane groundPlane;
+
+    private void Start()
+    {
+        this.agent = GetComponent<NavMeshAgent>();
+        this.groundPlane.SetNormalAndPosition(Vector3.up, Vector3.zero);
+
+        sceneCamera = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Calcular Posicion
+           Ray ray = sceneCamera.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            
+            groundPlane.Raycast(ray, out distance);
+            Vector3 point = ray.GetPoint(distance);
+
+            this.agent.SetDestination(point);
+
+
+        }
+    }
+}
